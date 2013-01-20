22dc40f3bfb9aaddb5406ce7ef33a57eeb354873
Samir Talwar <stalwar@palantir.com>
2013-01-20 20:24:33 +0100

    Exercise 1.2.

---
+++ test/sicp/chapter_1/exercise_2.clj
@@ -0,0 +1,9 @@
+(ns sicp.chapter-1.exercise-2
+  (:use clojure.test))
+
+(def answer (/ -37 150))
+
+(deftest exercise-2
+         (testing (is (= (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+                            (* 3 (- 6 2) (- 2 7)))
+                         answer))))
