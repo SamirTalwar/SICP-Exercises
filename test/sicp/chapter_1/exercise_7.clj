33853103aca865d099820696637811f971241c09
Samir Talwar <stalwar@palantir.com>
2013-01-21 06:46:25 +0100

    Exercise 1.7.

---
+++ test/sicp/chapter_1/exercise_7.clj
@@ -0,0 +1,34 @@
+(ns sicp.chapter-1.exercise-7
+  (:use clojure.test))
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn square [x] (* x x))
+
+(defn good-enough? [guess previous-guess]
+  (< (Math/abs (- guess previous-guess)) 0.001))
+
+(defn sqrt-iter [guess previous-guess x]
+  (if (good-enough? guess previous-guess)
+    guess
+    (sqrt-iter (improve guess x) guess x)))
+
+(defn sqrt [x]
+  (sqrt-iter 1.0 0.0 x))
+
+
+(defn almost-equal [a b]
+  (< (Math/abs (- a b)) 0.001))
+
+(deftest exercise-7
+         (is (almost-equal 3.0 (sqrt 9)))
+         (is (almost-equal 11.7047 (sqrt (+ 100 37))))
+         (is (almost-equal 1.7738 (sqrt (+ (sqrt 2) (sqrt 3)))))
+         (is (almost-equal 1000.0 (square (sqrt 1000))))
+         ; The following tests do not work with the original `sqrt` implementation.
+         (is (almost-equal 0.001 (sqrt 0.000001)))
+         (is (almost-equal 3037000499.97605 (sqrt Long/MAX_VALUE))))
