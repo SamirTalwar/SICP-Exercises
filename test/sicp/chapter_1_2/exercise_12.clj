5114028dabbc6132dd2413ed69a7fe7e3d938a7a
Samir Talwar <stalwar@palantir.com>
2013-02-04 15:33:42 +0000

    Exercise 1.12.

---
+++ test/sicp/chapter_1_2/exercise_12.clj
@@ -0,0 +1,21 @@
+(ns sicp.chapter-1-2.exercise-12
+  (:use clojure.test))
+
+(defn pascal [row column]
+  (cond (< column 1) 0
+        (> column row) 0
+        (= row 1) 1
+        :else (+ (pascal (- row 1) (- column 1)) (pascal (- row 1) column))
+        ))
+
+(deftest exercise-12
+         (is (= 1 (pascal 1 1)))
+         (is (= 1 (pascal 2 1)))
+         (is (= 1 (pascal 2 2)))
+         (is (= 2 (pascal 3 2)))
+         (is (= 3 (pascal 4 2)))
+         (is (= 1 (pascal 4 4)))
+         (is (= 6 (pascal 5 3)))
+         (is (= 10 (pascal 6 4)))
+         (is (= 5 (pascal 6 5)))
+         (is (= 1 (pascal 6 6))))
