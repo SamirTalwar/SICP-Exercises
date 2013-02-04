850175d741c9957348a81db23711a3695674afa7
Samir Talwar <stalwar@palantir.com>
2013-02-04 16:43:30 +0000

    Exercise 1.17.

--- test/sicp/chapter_1_2/exercise_17.clj
+++ test/sicp/chapter_1_2/exercise_17.clj
@@ -1,17 +1,14 @@
-(ns sicp.chapter-1-2.exercise-18
+(ns sicp.chapter-1-2.exercise-17
   (:use clojure.test))
 
 (defn multiply [b n]
-  (defn multiply-iter [a b n]
-    (defn double-value [x] (+ x x))
-    (defn halve [x] (/ x 2))
-    (cond (= n 0) 0
-          (= n 1) (+ a b)
-          (even? n) (multiply-iter a (double-value b) (halve n))
-          :else (multiply-iter (+ b a) b (- n 1))))
-  (multiply-iter 0 b n))
+  (defn double-value [x] (+ x x))
+  (defn halve [x] (/ x 2))
+  (cond (= n 0) 0
+        (even? n) (double-value (multiply b (halve n)))
+        :else (+ b (multiply b (- n 1)))))
 
-(deftest exercise-18
+(deftest exercise-17
          (is (= 1 (multiply 1 1)))
          (is (= 3 (multiply 1 3)))
          (is (= 6 (multiply 6 1)))
