a6cda36013e3a51145b8d946382c79e10b9cbd5c
Samir Talwar <stalwar@palantir.com>
2013-02-07 21:54:51 +0000

    Exercise 1.27.

---
+++ test/sicp/chapter_1_2/exercise_27.clj
@@ -0,0 +1,24 @@
+(ns sicp.chapter-1-2.exercise-27
+  (:use clojure.test)
+  (:require [clojure.string :as string]))
+
+(defn square [n] (* n n))
+
+(defn expmod [base exp m]
+  (cond (= exp 0) 1
+        (even? exp) (rem (square (expmod base (/ exp 2) m)) m)
+        :else (rem (* base (expmod base (- exp 1) m)) m)))
+
+(defn fermat-test [n test-value]
+  (= (expmod test-value n n) test-value))
+
+(defn carmichael [n]
+  (every? #(fermat-test n %) (range 1 n)))
+
+(deftest exercise-27
+         (is (carmichael 561))
+         (is (carmichael 1105))
+         (is (carmichael 1729))
+         (is (carmichael 2465))
+         (is (carmichael 2821))
+         (is (carmichael 6601)))
