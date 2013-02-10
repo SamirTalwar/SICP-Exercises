f6649c236bb76ff72f39fab5b09ae13b0b3a3a63
Samir Talwar <stalwar@palantir.com>
2013-02-11 00:41:47 +0100

    Exercise 1.30.

---
+++ test/sicp/chapter_1_3/exercise_30.clj
@@ -0,0 +1,30 @@
+(ns sicp.chapter-1-3.exercise-30
+  (:use clojure.test))
+
+(defn sum [term a next-value b]
+  (defn iter [a result]
+    (if (> a b)
+      result
+      (iter (next-value a) (+ result (term a)))))
+  (iter a 0))
+
+(defn sum-cubes [a b]
+  (sum (fn [n] (* n n n)) a inc b))
+
+(defn sum-integers [a b]
+  (sum identity a inc b))
+
+(defn pi-sum [a b]
+  (defn pi-term [x]
+    (/ 1.0 (* x (+ x 2))))
+  (defn pi-next [x]
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(defn approximately? [a b]
+  (< (Math/abs (- a b)) 0.01))
+
+(deftest exercise-30
+         (is (= 3025 (sum-cubes 1 10)))
+         (is (= 55 (sum-integers 1 10)))
+         (is (approximately? Math/PI (* 8 (pi-sum 1 1000)))))
