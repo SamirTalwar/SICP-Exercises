17a7ef1298d10ad8c77198956f052826b4f9a6a8
Samir Talwar <stalwar@palantir.com>
2013-02-11 18:40:19 +0100

    Exercise 1.33.

---
+++ test/sicp/chapter_1_3/exercise_33.clj
@@ -0,0 +1,35 @@
+(ns sicp.chapter-1-3.exercise-33
+  (:use clojure.test))
+
+(defn smallest-divisor [n]
+  (defn divides? [a b]
+    (= (rem b a) 0))
+  (defn find-divisor [n test-divisor]
+    (cond (> (* test-divisor test-divisor) n) n
+          (divides? test-divisor n) test-divisor
+          :else (find-divisor n (+ test-divisor 1))))
+  (find-divisor n 2))
+
+(defn prime? [n]
+  (= n (smallest-divisor n)))
+
+(defn coprime? [a b]
+  (= 1 (.gcd (BigInteger/valueOf a) (BigInteger/valueOf b))))
+
+
+(defn filtered-accumulate [predicate combiner null-value term a next-value b]
+  (if (> a b)
+    null-value
+    (combiner
+      (if (predicate a) (term a) null-value)
+      (filtered-accumulate predicate combiner null-value term (next-value a) next-value b))))
+
+(defn sum-of-squares-of-primes [a b]
+  (filtered-accumulate prime? + 0 (fn [n] (* n n)) a inc b))
+
+(defn product-of-smaller-coprimes [n]
+  (filtered-accumulate (partial coprime? n) * 1 identity 1 inc (- n 1)))
+
+(deftest exercise-33
+         (is (= 499269 (sum-of-squares-of-primes 100 200)))
+         (is (= 385 (product-of-smaller-coprimes 12))))
