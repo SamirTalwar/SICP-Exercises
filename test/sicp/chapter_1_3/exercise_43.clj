8a19fb926fcf73c2a20612f83069aab570657b20
Samir Talwar <stalwar@palantir.com>
2013-02-13 22:06:06 +0100

    Exercise 1.43.

---
+++ test/sicp/chapter_1_3/exercise_43.clj
@@ -0,0 +1,19 @@
+(ns sicp.chapter-1-3.exercise-43
+  (:use clojure.test))
+
+(defn compose [f g]
+  (fn [x] (f (g x))))
+
+(defn repeated [f n]
+  ((fn [func times]
+     (if (= times 1)
+       func
+       (recur (compose f func) (dec times))))
+     f n))
+
+(defn square [x]
+  (* x x))
+
+(deftest exercise-43
+         (is (= 20 ((repeated inc 10) 10)))
+         (is (= 625 ((repeated square 2) 5))))
