a701c6984175e53d30bdd1cda851bfa97c1e9c4e
Samir Talwar <stalwar@palantir.com>
2013-04-15 22:42:34 +0100

    Realised the `all-equal?` function is useless.

--- test/sicp/chapter_1_2/exercise_20.clj
+++ test/sicp/chapter_1_2/exercise_20.clj
@@ -6,16 +6,14 @@
     a
     (gcd b (rem a b))))
 
-(def all-equal? (partial apply =))
-
 (deftest exercise-20
-         (is (all-equal? [(gcd 206 40)
-                          (gcd 40 (rem 206 40))
-                          (gcd 6 (rem 40 6))
-                          (gcd 4 (rem 6 4))
-                          (gcd 2 (rem 4 2))
-                          (gcd 2 0)
-                          2])))
+         (is (= (gcd 206 40)
+                (gcd 40 (rem 206 40))
+                (gcd 6 (rem 40 6))
+                (gcd 4 (rem 6 4))
+                (gcd 2 (rem 4 2))
+                (gcd 2 0)
+                2)))
 
 ; Normal-order evaluation: There are four calls to `remainder`.
 ; Applicative-order evaluation: Infinite recursion, and so there would be theoretically infinite calls to `remainder`.
