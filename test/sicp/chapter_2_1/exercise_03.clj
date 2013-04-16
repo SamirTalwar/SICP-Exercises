e73bb1a8a325774467baff8bcdbecc6f611e8439
Samir Talwar <stalwar@palantir.com>
2013-04-16 10:11:09 +0100

    Ignored the book for Exercise 2.3 and stopped using coordinates.

--- test/sicp/chapter_2_1/exercise_03.clj
+++ test/sicp/chapter_2_1/exercise_03.clj
@@ -2,28 +2,26 @@
   (:use clojure.test)
   (:require [clojure.contrib.math :as math]))
 
-(defrecord Point [x y])
-
 (defprotocol Shape
   (perimeter [this])
   (area [this]))
 
-(defrecord Rectangle [top-left top-right bottom-right bottom-left]
+(defrecord Rectangle [top right bottom left]
   Shape
   (perimeter [this]
-    (def width (- (.x top-right) (.x top-left)))
-    (def height (- (.y top-left) (.y bottom-left)))
+    (def width (- right left))
+    (def height (- top bottom))
     (+ width height width height))
   (area [this]
-    (def width (- (.x top-right) (.x top-left)))
-    (def height (- (.y top-left) (.y bottom-left)))
+    (def width (- right left))
+    (def height (- top bottom))
     (* width height)))
 
 (deftest exercise-03
-         (def rectangle-a (Rectangle. (Point. 5 10) (Point. 10 10) (Point. 10 5) (Point. 5 5)))
+         (def rectangle-a (Rectangle. 10 10 5 5))
          (is (= 20 (.perimeter rectangle-a)))
          (is (= 25 (.area rectangle-a)))
 
-         (def rectangle-b (Rectangle. (Point. -3 2) (Point. 6 2) (Point. 6 -9) (Point. -3 -9)))
+         (def rectangle-b (Rectangle. 2 6 -9 -3))
          (is (= 40 (.perimeter rectangle-b)))
          (is (= 99 (.area rectangle-b))))
