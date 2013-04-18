7706600a2591bc794d0bd6b7887dfb66e92b2ff7
Samir Talwar <stalwar@palantir.com>
2013-04-18 17:05:50 +0100

    Use `reify` to avoid making calculations more than once.

--- test/sicp/chapter_2_1/exercise_03.clj
+++ test/sicp/chapter_2_1/exercise_03.clj
@@ -6,22 +6,20 @@
   (perimeter [this])
   (area [this]))
 
-(defrecord Rectangle [top right bottom left]
-  Shape
-  (perimeter [this]
-    (def width (- right left))
-    (def height (- top bottom))
-    (+ width height width height))
-  (area [this]
-    (def width (- right left))
-    (def height (- top bottom))
-    (* width height)))
+(defn rectangle [top right bottom left]
+  (def width (- right left))
+  (def height (- top bottom))
+  (reify Shape
+    (perimeter [this]
+      (+ width height width height))
+    (area [this]
+      (* width height))))
 
 (deftest exercise-03
-         (def rectangle-a (Rectangle. 10 10 5 5))
+         (def rectangle-a (rectangle 10 10 5 5))
          (is (= 20 (.perimeter rectangle-a)))
          (is (= 25 (.area rectangle-a)))
 
-         (def rectangle-b (Rectangle. 2 6 -9 -3))
+         (def rectangle-b (rectangle 2 6 -9 -3))
          (is (= 40 (.perimeter rectangle-b)))
          (is (= 99 (.area rectangle-b))))
